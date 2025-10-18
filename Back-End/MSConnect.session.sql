53da50dfa02596ff9d153cf4c4bafeae41a29def
Julio César <juliocesar.ti.2015@hotmail.com>
2025-10-18 19:36:21 -0400

    delete info, databaseConect

--- Back-End/MSConnect.session.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users (id, name, email, phone, created_at, updated_at)
-VALUES (
-    id:int,
-    'name:varchar',
-    'email:varchar',
-    'phone:varchar',
-    'created_at:timestamp',
-    'updated_at:timestamp'
-  );
